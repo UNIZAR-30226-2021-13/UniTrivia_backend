5ba643bda31b21cec1865580d71ed88f8bad0c58
Daniel Benedí García <danielillo2000@hotmail.com>
2021-03-24 18:09:35 +0100

    Añadido llamada a la BD para crear usuario

--- utils/DatabaseConnection.js
+++ utils/DatabaseConnection.js
@@ -11,4 +11,50 @@ function terminar() {
     cliente.close();
 }
 
-module.exports = {iniciar, terminar};
+/***
+ * Función para crear un usuario en la base de datos inicializada anteriormente.
+ *
+ * @param username Nombre de usuario a crear.
+ * @param hash Hash de la contraseña del usuario a crear.
+ * @param email Email del usuario a crear
+ * @param pregunta Pregunta de seguridad del usuario a crear.
+ * @param respuesta Respuesta a la pregunta de seguridad para el usuario que se va a crear.
+ * @returns {integer} 0 si ha podido insertar al usuario, 1 si existe el username y 2 error en la bd.
+ */
+function registrar(username, hash, email, pregunta, respuesta){
+    const usuario = {
+        _id: username,
+        mail: email,
+        hash: hash,
+        preg: pregunta,
+        res: respuesta
+    }
+    usuarios = bd.collection("usuarios");
+    var ok = 0;
+    try{
+        res = usuarios.find({_id:username});
+        res.count(true, {limit: 1}, function(err, count){
+            if(err){
+                ok = 2;
+                logger.error("Error creando un usuario.",err);
+            }else{
+                if(count === 0){
+                    usuarios.insertOne(usuario, function(err, res){
+                        if(err){
+                            ok = false;
+                            logger.error("Error creando un usuario.",err);
+                        }
+                        logger.info("Usuario "+username+" creado correctamente");
+                    });
+                }else{
+                    ok = 1;
+                }
+            }
+        });
+    }catch (err){
+        return 2;
+    }
+    return ok;
+}
+
+module.exports = {iniciar, terminar,registrar};
