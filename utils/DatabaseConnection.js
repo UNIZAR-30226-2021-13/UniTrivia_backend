59be088fdb94086518ae7f40095fc9b675832dd6
DiegoBielsa <776564@unizar.es>
2021-03-25 21:14:58 +0100

    Implementadas en primera instancia mis tres funciones

--- utils/DatabaseConnection.js
+++ utils/DatabaseConnection.js
@@ -19,4 +19,125 @@ function getBD(){
     return bd;
 }
 
-module.exports = {iniciar, terminar, getBD};
+/***
+ * Función para obtener la pregunta de seguridad de un usuario
+ *
+ *
+ * @param username Nombre del usuario del cual recuperar la pregunta
+ * @returns {string} devuelve la pregunta si esta se ha obtenido exitosamente y la cadena
+ * vacia en caso contrario
+ *
+ */
+function obtener_Pregunta(username){
+
+    let pregunta = "";
+
+    try{
+        if(bd === null){
+            logger.error("bd is null");
+            return pregunta;
+        }
+        const usuarios = bd.collection("usuarios");
+        usuarios.findOne({ _id:username }, function(err, user){
+            if( err ){
+                logger.error("Error obtener_Pregunta: error en la BD.", err);
+                return "";
+            }
+            if( !user ){
+                logger.error("Error obtener_Pregunta: no existe el usuario.", err);
+                return "";
+            }
+
+            pregunta = user.preg;
+
+        });
+
+    }catch (err){
+        logger.error("Unknown error", err);
+        return 2;
+    }
+
+    return pregunta;
+}
+
+/**
+ * Función para obtener la lista de ids de los objetos comprados del usuario en cuestión
+ *
+ * @param  username Nombre del usuario del cual recuperar la lista de objetos comprados
+ * @return {number[]} devuelve la lista de objetos en caso correcto, [-1] en caso de error en la bd y
+ *             [-2] si no existe el usuario
+ */
+function lista_comprados(username){
+    let list = [];
+
+    try{
+        if(bd === null){
+            logger.error("bd is null");
+            return [-1];
+        }
+        const usuarios = bd.collection("usuarios");
+        usuarios.findOne({ _id:username }, function(err, user){
+            if( err ){
+                logger.error("Error lista_comprados: error en la BD.", err);
+                return [-1];
+            }
+            if( !user ){
+                logger.error("Error lista_comprados: no existe el usuario.", err);
+                return [-2];
+            }
+
+            list = user.rfs;
+        });
+
+    }catch (err){
+        logger.error("Unknown error", err);
+        return [-1];
+    }
+
+    return list;
+}
+
+
+/**
+ * Función para validar la respuesta dada del usuario en cuestion
+ *
+ * @param username Nombre del usuario
+ * @param res Respuesta dada
+ * @return {number} 0 si la respuesta es correcta, 1 si es incorrecta y 2 si error
+ *          en la base de datos
+ */
+function validar_respuesta(username, res){
+    let ok = 0;
+
+    try{
+        if(bd === null){
+            logger.error("bd is null");
+            return 2;
+        }
+        const usuarios = bd.collection("usuarios");
+        usuarios.findOne({ _id:username }, function(err, user){
+            if( err ){
+                logger.error("Error validar_respuesta: error en la BD.", err);
+                return 2;
+            }
+            if( !user ){
+                logger.error("Error validar_respuesta: no existe el usuario.", err);
+                return 2;
+            }
+
+            if( res.toUpperCase() === user.res.toUpperCase() ){
+                ok = 0;
+            }else{
+                ok = 1;
+            }
+        });
+
+    }catch (err){
+        logger.error("Unknown error", err);
+        return 2;
+    }
+
+    return ok;
+}
+
+module.exports = {iniciar, terminar, getBD, obtener_Pregunta, lista_comprados, validar_respuesta};
