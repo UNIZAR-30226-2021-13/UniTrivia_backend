718d29be370d9aa0b121c1eb559a84bc4fa7ff01
alej008 <775929@unizar.es>
2021-04-02 01:10:35 +0200

    Algunas funciones de la memoria implementadas, pero añun no testeadas.

--- utils/ServerCache.js
+++ utils/ServerCache.js
@@ -45,12 +45,25 @@ class NodoSala{
      *
      * @param {string[]} jugadores Array con el nombre de usuario de cada jugador
      * @param {number} nJugadores Número de componentes del array jugadores
+     * @param {string} lider El nombre de usuario del líder de la sala
      */
-    constructor(jugadores, nJugadores){
+    constructor(jugadores, nJugadores, lider){
         this.jugadores = jugadores;
         this.nJugadores = nJugadores;
+        this.lider = lider;
         this.mutex = new Mutex();
     }
+}
+
+
+function randString(length) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
 }
 
 /**
@@ -86,18 +99,72 @@ function stop() {
 
 /**
  * Función para crear una sala con el líder de la sala
- * @param usuario
+ * @param {string} usuario Usuario líder de la sala
+ * @param {boolean} priv True si la sala es privada y false en caso contrario
  */
-function crearSala(usuario){
+function crearSala(usuario, priv){
+    try{
+        let id_sala = randString(5);
+        let valor = NodoSala([usuario], 1, usuario);
+        if(priv){
+            salasPriv.set(id_sala, valor);
+        } else {
+            salasPub.set(id_sala, valor);
+        }
+        return 0;
+
+    } catch (e){
+        logger.error('Error al crear sala', e)
+        return 1;
+    }
+
 
 }
 
 /**
  * Función para que un jugador se pueda unir a una sala
- * @param id_sala
- * @param usuario
+ * @param {string} id_sala Identificador de la sala
+ * @param {string} usuario Usuario a añadir a la sala
+ * @param {boolean} priv True si la sala es privada y false en caso contrario
  */
-function unirseSala(id_sala, usuario){
+async function unirseSala(id_sala, usuario, priv){
+    try{
+        let value = salasPriv.get(id_sala);
+        if(priv){
+            if(value === undefined){
+                return 1;
+            } else {
+                return await value.mutex.runExclusive(async () => {
+                    if(value.nJugadores < 6) {
+                        value.nJugadores++;
+                        value.jugadores.push(usuario);
+                        return 0;
+                    } else {
+                        return 1;
+                    }
+                });
+
+            }
+        } else {
+            value = salasPub.get(id_sala);
+            if(value === undefined){
+                return 1;
+            } else {
+                return await value.mutex.runExclusive(async () => {
+                    if(value.nJugadores < 6) {
+                        value.nJugadores++;
+                        value.jugadores.push(usuario);
+                        return 0;
+                    } else {
+                        return 1;
+                    }
+                });
+            }
+        }
+    } catch (e){
+        logger.error('Error al unirse a la sala', e);
+        return 1;
+    }
 
 }
 
@@ -111,11 +178,58 @@ function buscarPartida(usuario){
 
 /**
  * Función para obandonar la sala o echar a un jugador de ella
- * @param id_sala
- * @param usuario
+ * @param {string} id_sala Identificador de la sala
+ * @param {string} usuario Usuario a echar de la sala
+ * @param {boolean} priv True si la sala es privada y false en caso contrario
  */
-function abandonarSala(id_sala, usuario){
-
+async function abandonarSala(id_sala, usuario, priv){
+    try{
+        let value = salasPriv.get(id_sala);
+        if(priv){
+            if(value === undefined){
+                return 1;
+            } else {
+                return await value.mutex.runExclusive(async () => {
+                    value.nJugadores--;
+                    let index = -1;
+                    for (let i = 0; i < value.jugadores.length && index < 0; i++) {
+                        if (value.jugadores[i] === usuario) {
+                            index = i;
+                        }
+                    }
+                    if (index >= 0) {
+                        value.jugadores.splice(index, 1);
+                        return 0;
+                    } else{
+                        return 1;
+                    }
+                });
+            }
+        } else {
+            value = salasPub.get(id_sala);
+            if(value === undefined){
+                return 1;
+            } else {
+                return await value.mutex.runExclusive(async () => {
+                    value.nJugadores--;
+                    let index = -1;
+                    for (let i = 0; i < value.jugadores.length && index < 0; i++) {
+                        if (value.jugadores[i] === usuario) {
+                            index = i;
+                        }
+                    }
+                    if (index >= 0) {
+                        value.jugadores.splice(index, 1);
+                        return 0;
+                    } else{
+                        return 1;
+                    }
+                });
+            }
+        }
+    } catch (e) {
+        logger.error('Error al abandonar sala', e)
+    }
 }
 
 /**
@@ -135,17 +249,40 @@ function comenzarPartida(id_sala){
 }
 
 /**
- * Función para actualizar la casilla de un jugador
- * @param id_partida
- * @param jugador
- * @param casilla
+ * Función para actualizar la casilla de un jugador en su turno
+ * @param {string} id_partida
+ * @param {string} jugador
+ * @param {number[]} casilla Nuevas posiciones del jugador
  */
 function actualizarCasilla(id_partida, jugador, casilla){
-
+    try{
+        let value = salasJuego.get(id_partida);
+        if(value !== undefined){
+            if(value.turno === jugador){
+                let actualizado = false;
+                for(let i = 0; i<value.jugadores.length && !actualizado; i++){
+                    if(value.jugadores[i].nombre === jugador){
+                        value.jugadores[i].casilla = casilla;
+                        actualizado = true;
+                    }
+                }
+                return actualizado ? 0 : 1;
+            } else {
+                logger.error('Error al actualizar casilla, no es el turno del jugador');
+                return 2;
+            }
+        } else {
+            logger.error('Error al actualizar casilla, no existe la sala');
+            return 1;
+        }
+    } catch (e) {
+        logger.error('Error al actualizar casilla', e);
+        return 1;
+    }
 }
 
 /**
- * Función para añadir el quesito conseguido a un jugador
+ * Función para añadir el quesito conseguido a un jugador en su turno
  * @param id_partida
  * @param jugador
  * @param quesito
