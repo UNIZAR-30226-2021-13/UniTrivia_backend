51da9a9f345ed0a212f599b1ce0abfca5cba4277
Daniel Benedí García <774156@unizar.es>
2021-04-01 17:22:17 +0200

    Preguntas y script poblar preguntas. No está traducido
    Signed-off-by: Daniel Benedí García <774156@unizar.es>

---
+++ scripts/poblar_preguntas.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+############################
+# USAGE
+############################
+usage(){
+	echo "Usage: $0 [-h <host>] [-p <port>]"
+	exit 1
+}
+
+PORT="27017"
+HOST="localhost"
+if [ "$#" -ne 0 ] && [ "$#" -ne 2 ] && [ "$#" -ne 4 ]; then
+	usage
+elif [ "$#" -eq 2 ] && [ "$1" != "-h" ] && [ "$1" != "-p" ]; then
+	usage
+elif [ "$#" -eq 4 ] && [ "$1" != "-h" ] && [ "$1" != "-p" ] && [ "$2" != "-h" ] && [ "$2" != "-p" ]; then
+	usage
+fi
+
+if [ "$1" == "-h" ]; then
+	HOST="$2"
+elif [ "$2" == "-h" ]; then
+	HOST="$4"
+fi
+if [ "$1" == "-p" ]; then
+	PORT="$2"
+elif [ "$2" == "-p" ]; then
+	PORT="$4"
+fi
+
+
+if [ "$EUID" -ne 0 ]; then
+	echo "Please run as root"
+	exit
+fi
+
+if ! pgrep mongod > /dev/null; then
+	echo "Mongod must be running"
+	exit
+fi
+
+if ! command -v mongoimport &> /dev/null
+then
+    echo "mongoimport could not be found. Install mongo-tools for example with apt."
+    exit
+fi
+
+
+mongoimport --host "$HOST" --port "$PORT" \
+            --db="UniTrivia" --collection="preguntas" \
+            --jsonArray --file="preguntas.json"
