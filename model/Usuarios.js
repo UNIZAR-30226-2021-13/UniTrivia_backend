463efaedf30915ee9c6fc16fb57cdc60e612a5c8
DiegoBielsa <71701253+DiegoBielsa@users.noreply.github.com>
2021-04-05 18:57:17 +0200

    Update Usuarios.js
    Se ha añadido la función para validar respuesta

--- model/Usuarios.js
+++ model/Usuarios.js
@@ -352,5 +352,40 @@ async function deletePerfil(token){
     }
 }
 
+/**
+ * Función para validar la respuesta dada del usuario en cuestion
+ *
+ * @param token Token de sesión del usuario
+ * @param res Respuesta dada
+ * @return {number} 0 si la respuesta es correcta, 1 si es incorrecta, 2 si error
+ *          en la base de datos y 3 si acceso no autorizado
+ */
+async function validar_respuesta(token, res){
+    let ok = 0;
+
+    try{
+        const usuarios = db.getBD().collection("usuarios");
+        const obj = validarToken(token);
+        if( obj ){
+            const user = await usuarios.findOne({_id: obj.user});
+
+
+            if( res.toUpperCase() === user.res.toUpperCase() ){
+                ok = 0;
+            }else{
+                ok = 1;
+            }
+        }else{
+            logger.error("Usuario no identificado");
+            ok = 3;
+        }
+    }catch(e){
+        logger.error("Error al validar respuesta: error en la BD.", e);
+        ok = 2;
+    }
+
+    return ok;
+}
+
 module.exports = {registrar, logear, modificar_ficha, modificar_banner,
-                    modificar_avatar, modificar_pass,getPerfil, deletePerfil}
+                    modificar_avatar, modificar_pass,getPerfil, deletePerfil, validar_respuesta}
