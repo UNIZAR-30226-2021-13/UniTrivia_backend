6a99ea3250ace90faf79fb90afd8928b2060f88b
Daniel Benedí García <774156@unizar.es>
2021-04-16 17:44:19 +0200

    Introducido el frontend

---
+++ build/precache-manifest.40d27ecf5138c6ea773b123b17e7e15b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c1e7328c79447f8dabe468d1ef050a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f3d4cc0bbef4a138810",
+    "url": "/static/css/main.3811a24b.chunk.css"
+  },
+  {
+    "revision": "efd417d83818d87a9173",
+    "url": "/static/js/2.a9727252.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.a9727252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f3d4cc0bbef4a138810",
+    "url": "/static/js/main.d79ccd9c.chunk.js"
+  },
+  {
+    "revision": "c831ac213bed9e4d0c50",
+    "url": "/static/js/runtime-main.9c055711.js"
+  }
+]);
