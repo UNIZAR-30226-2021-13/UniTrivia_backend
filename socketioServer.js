d6b5976a918d8a69edd63c471d083eec3c1eb171
Daniel Benedí García <774156@unizar.es>
2021-05-24 09:43:01 +0200

    Añadido handler de connection_error

--- socketioServer.js
+++ socketioServer.js
@@ -37,6 +37,15 @@ class SocketioServer{
             }
         });
 
+        this.io.engine.on("connection_error", (err) => {
+            console.log("connection_error");
+            console.log(err.req);	     // the request object
+            console.log(err.code);     // the error code, for example 1
+            console.log(err.message);  // the error message, for example "Session ID unknown"
+            console.log(err.context);  // some additional error context
+            console.log("\n");
+        });
+
         this.io.on('connection',  async (socket) => {
 
             const operacion = socket.request.headers['operacion'];
